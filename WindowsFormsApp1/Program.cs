3e24362cddc132096c0e27ba55bd5059ecc6b5bd
Eterniitys <johan.guerrero394@gmail.com>
2020-04-20 13:02:46 +0200

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	static class Program
+	{
+		/// <summary>
+		/// Point d'entrée principal de l'application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var thread = new Thread(mapView);
+			Application.Run(new MapView());
+		}
+
+		private static void mapView()
+		{
+			Application.Run(new MapView());
+		}
+	}
+}
